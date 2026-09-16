14d1bbbdeb67e3b9dc61cc632adc66f0d594d5ad
augusto-laram <augustolara304@gmail.com>
2026-09-16 17:18:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+email = os.environ.get("MY_EMAIL")
+PASSWORD = os.environ.get("MY_PASSWORD")
 letters = ['letter_1.txt','letter_2.txt','letter_3.txt']
 def get_letter(date):
     person_file = birthdays[(birthdays.month==date.month) & (birthdays.day ==date.day)]
